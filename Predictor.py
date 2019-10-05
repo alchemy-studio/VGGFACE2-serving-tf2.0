7aaf5f438034d805c1d2433fd8a5dd92fd3c91ff
breadbread1984 <breadbread1984@163.com>
2019-10-05 15:25:17 +0800

    add files

---
+++ Predictor.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from os.path import join;
+from math import ceil;
+import numpy as np;
+import cv2;
+import tensorflow as tf;
+
+class Predictor(object):
+
+    def __init__(self, model_path = 'models'):
+
+        self.model = tf.keras.models.load_model(join(model_path, 'weights.h5'), compile = False);
+
+    def preprocess(self, img):
+
+        assert img.shape[2] == 3;
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB);
+        inputs = tf.expand_dims(img, axis = 0);
+        outputs = tf.image.resize_with_crop_or_pad(inputs, 224, 224);
+        return outputs;
+
+    def batch(self, imgs):
+
+        inputs = [self.preprocess(img) for img in imgs];
+        outputs = tf.concat(inputs, axis = 0);
+        return outputs;
+
+    def encoding(self, imgs):
+
+        assert type(imgs) is list;
+        assert np.all([type(img) is np.ndarray and len(img.shape) == 3 for img in imgs]);
+        batch = self.batch(imgs);
+        return self.model(batch);
+
+    def template_encoding(self, imgs):
+
+        assert type(imgs) is list;
+        assert np.all([type(img) is np.ndarray and len(img.shape) == 3 for img in imgs]);
+        
+
+if __name__ == "__main__":
+
+    assert tf.executing_eagerly() == True;
+    predictor = Predictor();
