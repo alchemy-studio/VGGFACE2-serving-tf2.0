5af2dfde3cccd74c35eec306b4880c0783a7cb21
breadbread1984 <breadbread1984@163.com>
2019-10-06 13:58:09 +0800

    update reconizer

--- OfflineRecognizer.py
+++ OfflineRecognizer.py
@@ -26,9 +26,9 @@ class Recognizer(object):
 
     def loadFaceDB(self, directory = 'facedb'):
 
-        if False == exists(directory) || False == isdir(dirrectory):
+        if False == exists(directory) or False == isdir(directory):
             print("invalid directory");
-            return;
+            return False;
         self.ids = dict();
         count = 0;
         features = tf.zeros((0,8631), dtype = tf.float32);
@@ -46,23 +46,23 @@ class Recognizer(object):
                         image = cv2.imread(join(directory,f,img));
                         if image is None:
                             print("can't open file " + join(directory,f,img));
-                            return;
+                            continue;
                         rectangles = self.detector.detect(image);
                         if rectangles.shape[0] != 1:
                             print("can't detect single face in image " + join(directory,f,img));
-                            return;
+                            continue;
                         # crop square from facial area
                         upperleft = rectangles[0,0:2];
                         downright = rectangles[0,2:4];
                         wh = downright - upperleft;
-                        length = np.max(wh);
-                        center = np.round((upperleft + downright) / 2);
-                        upperleft = center - np.round(np.array([length,length]) / 2);
-                        downright = upperleft + np.array([length,length]);
+                        length = tf.math.reduce_max(wh, axis = -1).numpy();
+                        center = (upperleft + downright) // 2;
+                        upperleft = center - tf.constant([length,length], dtype = tf.float32) // 2;
+                        downright = upperleft + tf.constant([length,length], dtype = tf.float32);
                         face = image[upperleft[1]:downright[1],upperleft[0]:downright[0],:];
                         imgs.append(face);
                 feature = self.encoder.encode(imgs);
-                label = tf.tile(tf.constant([[label]], dtype = tf.int32), (features.shape[0],1));
+                label = tf.tile(tf.constant([[label]], dtype = tf.int32), (feature.shape[0],1));
                 features = tf.concat([features,feature],axis = 0);
                 labels = tf.concat([labels, label], axis = 0);
         features = features.numpy(); # features.shape = (n, 8631)
@@ -74,6 +74,7 @@ class Recognizer(object):
         # save ids
         with open('ids.pkl', 'wb') as f:
             f.write(pickle.dumps(self.ids));
+        return True;
 
     def recognize(self, image):
 
@@ -84,13 +85,16 @@ class Recognizer(object):
         wh = downright - upperleft;
         length = tf.math.reduce_max(wh, axis = -1);
         center = (upperleft + downright) // 2;
-        upperleft = center - tf.stack([length,length], axis = -1);
+        upperleft = center - tf.stack([length,length], axis = -1) // 2;
         downright = upperleft + tf.stack([length, length], axis = -1);
         upperleft = tf.reverse(upperleft, axis = [1]); # in h,w order
         downright = tf.reverse(downright, axis = [1]); # in h,w order
         boxes = tf.concat([upperleft, downright], axis = -1) / tf.cast(tf.tile(image.shape[0:2], (2,)), dtype = tf.float32);
         image = tf.expand_dims(tf.cast(image, dtype = tf.float32), axis = 0);
         faces = tf.crop_and_resize(image, boxes, tf.zeros((boxes.shape[0],), dtype = tf.int32),(224,224));
+        for face in faces:
+            cv2.imshow('face', face.numpy().astype('uint8'));
+            cv2.waitKey();
         features = self.encoder.encode(faces);
         ret, results, neighbours, dist = self.knn.findNearest(features.numpy(), k = 1);
         # TODO
@@ -99,4 +103,5 @@ if __name__ == "__main__":
 
     assert tf.executing_eagerly() == True;
     recognizer = Recognizer();
+    recognizer.loadFaceDB();
 
